2cc173a2cdf413a1101b2a69bb2b2e1e2b5a3d80
player31-kks <keumks0331@gmail.com>
2023-08-03 18:37:45 +0900

    Add: AutoModule

---
+++ src/auto/decorator/AutoInjectable.spec.ts
@@ -0,0 +1,11 @@
+import { AUTO_INJECTABLE_WATERMARK } from '../constants';
+import { AutoInjectable } from './AutoInjectable';
+
+describe('AutoInjectable decorator', () => {
+  @AutoInjectable()
+  class Test {}
+
+  it('should add metadata to class', () => {
+    expect(Reflect.getMetadata(AUTO_INJECTABLE_WATERMARK, Test)).toBe(true);
+  });
+});
---
+++ test/fixture.ts
@@ -0,0 +1,4 @@
+import { AutoInjectable } from '../src/auto/decorator/AutoInjectable';
+
+@AutoInjectable()
+export class AutoInjectableFixture1 {}
