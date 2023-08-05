b8104047de9cdc100589c11b696e1668ce5f2f41
Nhahan <kisy324@naver.com>
2023-08-05 11:44:28 +0900

    refactor: remove unused INJECTABLE_WATERMARK

--- src/auto/decorator/AutoInjectable.ts
+++ src/auto/decorator/AutoInjectable.ts
@@ -1,10 +1,8 @@
 import 'reflect-metadata';
 import { AUTO_INJECTABLE_WATERMARK } from '../constants';
-import { INJECTABLE_WATERMARK } from '@nestjs/common/constants';
 
 export function AutoInjectable(): ClassDecorator {
   return (target: object) => {
-    Reflect.defineMetadata(INJECTABLE_WATERMARK, true, target);
     Reflect.defineMetadata(AUTO_INJECTABLE_WATERMARK, true, target);
   };
 }
