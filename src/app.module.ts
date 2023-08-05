450a89e40cc76d15f97af9f941f806fe55aa0c32
Nhahan <kisy324@naver.com>
2023-08-05 22:48:46 +0900

    refactor: library sources directory refactored auto -> lib

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppService } from './app.service';
 import { AppController } from './app.controller';
-import { ComponentScan } from './auto/decorator/component-scan.decorator';
+import { ComponentScan } from './lib/decorator/component-scan.decorator';
 
 @ComponentScan()
 @Module({
