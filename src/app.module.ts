327a2e76ded8ed5440f6b334229a1d05db93a633
Nhahan <kisy324@naver.com>
2023-08-05 11:57:30 +0900

    feat: @ComponentScan decodator settings

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AutoModule } from './auto/auto.module';
+import { ComponentScan } from './auto/decorator/ComponenScan';
 
+@ComponentScan()
 @Module({
-  imports: [AutoModule.forRootAsync(['dist/**/*.js'])],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
