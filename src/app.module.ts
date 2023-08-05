d1206cbb107215ba2fe385701676a6328615ac44
Nhahan <kisy324@naver.com>
2023-08-05 12:12:53 +0900

    fix: @ComponentScan has ['dist/**/*js'] as a default value

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ComponentScan } from './auto/decorator/ComponenScan';
+import { ComponentScan } from './auto/decorator/component-scan.decorator';
 
 @ComponentScan()
 @Module({
