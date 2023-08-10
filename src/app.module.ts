ad0b9fd60d82144b20f05ba1245793fe8ad2e553
Nhahan <kisy324@naver.com>
2023-08-10 21:57:23 +0900

    chore: import `component-scan` feature properly

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppService } from './app.service';
 import { AppController } from './app.controller';
-import {ComponentScan} from "../lib";
+import { ComponentScan } from '../lib';
 
 @ComponentScan()
 @Module({
