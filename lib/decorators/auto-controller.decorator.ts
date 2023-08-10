98171ed917c5a716f735971efd7055d0e38ee31e
Nhahan <kisy324@naver.com>
2023-08-11 01:00:39 +0900

    fix: @Provider -> @Injectable which is actually provider

--- lib/decorators/auto-controller.decorator.ts
+++ lib/decorators/auto-controller.decorator.ts
@@ -6,7 +6,7 @@ export function AutoController(): ClassDecorator {
     Reflect.defineMetadata(AUTO_CONTROLLER_WATERMARK, true, target);
 
     /**
-     * Unlike `@Provider`, in Nest, the presence of the `@Controller` decorator is essential
+     * Unlike `@Injectable`, in Nest, the presence of the `@Controller` decorator is essential
      * for defining request-handling controller classes. It designates routing and functionality,
      * and its absence leads to errors.
      */
