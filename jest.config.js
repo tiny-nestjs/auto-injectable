112368b927c11e3881bfeca3a300f70778816c9c
player31-kks <keumks789@naver.com>
2023-08-10 18:35:59 +0900

    fix: lib can be pulled into src

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   moduleFileExtensions: ['js', 'json', 'ts'],
-  rootDir: 'src',
+  rootDir: './',
   testRegex: '.*\\.spec\\.ts$',
   transform: {
     '^.+\\.(t|j)s$': 'ts-jest',
--- test/fixture.ts
+++
@@ -1,4 +0,0 @@
-import { AutoInjectable } from '../src/lib/decorators/auto-injectable.decorator';
-
-@AutoInjectable()
-export class AutoInjectableFixture {}
