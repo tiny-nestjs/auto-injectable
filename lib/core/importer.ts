8fd30c036fb2a1c6aee4dd0b61ef73b5eb29d85c
Nhahan <kisy324@naver.com>
2023-08-10 23:00:51 +0900

    feat: loadProviders, loadControllers Importer

--- lib/core/importer.ts
+++ lib/core/importer.ts
@@ -1,37 +1,76 @@
 import { glob } from 'glob';
 import { resolve } from 'path';
-import { AUTO_INJECTABLE_WATERMARK } from '../interfaces';
 
 type ClassType = new (...args: any[]) => any;
 
-export interface AutoImportResult {
+export interface AutoProviders {
   providers: ClassType[];
+}
+
+export interface AutoControllers {
+  controllers: ClassType[];
 }
 
 export class Importer {
   constructor(private readonly patterns: string[]) {}
 
-  static async load(patterns: string[]): Promise<AutoImportResult> {
+  static async loadProviders(
+    patterns: string[],
+    waterMark: symbol,
+  ): Promise<AutoProviders> {
     const importer = new Importer(patterns);
     const pathNames = await importer.matchGlob();
     const results = await Promise.all(
-      pathNames.map((pathName) => importer.importFile(pathName)),
+      pathNames.map((pathName) => importer.importProvider(pathName, waterMark)),
     );
     return {
       providers: results.flatMap((result) => result.providers),
     };
   }
 
-  public async importFile(pathName: string): Promise<AutoImportResult> {
+  static async loadControllers(
+    patterns: string[],
+    waterMark: symbol,
+  ): Promise<AutoControllers> {
+    const importer = new Importer(patterns);
+    const pathNames = await importer.matchGlob();
+    const results = await Promise.all(
+      pathNames.map((pathName) =>
+        importer.importController(pathName, waterMark),
+      ),
+    );
+    return {
+      controllers: results.flatMap((result) => result.controllers),
+    };
+  }
+
+  public async importProvider(
+    pathName: string,
+    waterMark: symbol,
+  ): Promise<AutoProviders> {
     const exports: Record<string, unknown> = await import(pathName);
     const providers = Object.values(exports)
       .filter((value) => typeof value === 'function')
       .filter((value) =>
-        Reflect.hasMetadata(AUTO_INJECTABLE_WATERMARK, value as ClassType),
+        Reflect.hasMetadata(waterMark, value as ClassType),
       ) as ClassType[];
-
     return {
       providers,
+    };
+  }
+
+  public async importController(
+    pathName: string,
+    waterMark: symbol,
+  ): Promise<AutoControllers> {
+    const exports: Record<string, unknown> = await import(pathName);
+    const controllers = Object.values(exports)
+      .filter((value) => typeof value === 'function')
+      .filter((value) =>
+        Reflect.hasMetadata(waterMark, value as ClassType),
+      ) as ClassType[];
+    return {
+      controllers,
     };
   }
 
