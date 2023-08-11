c2be8005ba79b83124d4406c89c0dadf8950a37f
Nhahan <kisy324@naver.com>
2023-08-11 23:40:01 +0900

    chore: options -> patterns

--- lib/decorators/component-scan.decorator.ts
+++ lib/decorators/component-scan.decorator.ts
@@ -2,21 +2,18 @@ import { AutoModule } from '../auto.module';
 import { Module } from '@nestjs/common';
 import * as path from 'path';
 
-export function ComponentScan(
-  paths: string[] = [getRootPath()],
-): ClassDecorator {
+export function ComponentScan(paths: string[] = [getRootPath()]): ClassDecorator {
   return function (target: any) {
     const originalMetadata = {
       imports: Reflect.getMetadata(MODULE_OPTIONS.IMPORTS, target) || [],
-      controllers:
-        Reflect.getMetadata(MODULE_OPTIONS.CONTROLLERS, target) || [],
+      controllers: Reflect.getMetadata(MODULE_OPTIONS.CONTROLLERS, target) || [],
       providers: Reflect.getMetadata(MODULE_OPTIONS.PROVIDERS, target) || [],
     };
 
-    const options = paths.map((path) => `${path}/**/*.js`);
+    const patterns = paths.map((path) => `${path}/**/*.js`);
     Module({
       ...originalMetadata,
-      imports: [...originalMetadata.imports, AutoModule.forRootAsync(options)],
+      imports: [...originalMetadata.imports, AutoModule.forRootAsync(patterns)],
     })(target);
   };
 }
