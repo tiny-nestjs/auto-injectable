a520fd7da2504a70065f77e352337d79baf00323
Nhahan <kisy324@naver.com>
2023-08-11 11:28:50 +0900

    fix: AutoController function now has `prefix: string | string[]` parameter

--- lib/decorators/auto-controller.decorator.ts
+++ lib/decorators/auto-controller.decorator.ts
@@ -1,7 +1,7 @@
 import { Controller } from '@nestjs/common';
 import { AUTO_CONTROLLER_WATERMARK } from '../interfaces';
 
-export function AutoController(): ClassDecorator {
+export function AutoController(prefix: string | string[]): ClassDecorator {
   return (target: object) => {
     Reflect.defineMetadata(AUTO_CONTROLLER_WATERMARK, true, target);
 
@@ -10,6 +10,6 @@ export function AutoController(): ClassDecorator {
      * the presence of the `@Controller()` decorator is essential
      * for defining request-handling controller classes.
      */
-    Controller()(target as new (...args: any[]) => any);
+    Controller(prefix)(target as new (...args: any[]) => any);
   };
 }
