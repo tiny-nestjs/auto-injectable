94d2f8145643cf396a2507b1946639af1a19b8dd
suny <suny@nm-metaworold.com>
2023-08-11 11:35:40 +0900

    refactor: AutoController() overloading

--- lib/decorators/auto-controller.decorator.ts
+++ lib/decorators/auto-controller.decorator.ts
@@ -1,17 +1,21 @@
-import { Controller, ControllerOptions } from "@nestjs/common";
+import { Controller, ControllerOptions } from '@nestjs/common';
 import { AUTO_CONTROLLER_WATERMARK } from '../interfaces';
 
-export function AutoController(
-  prefixOrOptions?: string | string[] | ControllerOptions,
+interface AutoControllerFn {
+  (prefix: string): ClassDecorator;
+  (prefixOrOptions: string[] | ControllerOptions): ClassDecorator;
+}
+
+export const AutoController: AutoControllerFn = function AutoController(
+  prefixOrOptions: string | string[] | ControllerOptions,
 ): ClassDecorator {
   return (target: object) => {
     Reflect.defineMetadata(AUTO_CONTROLLER_WATERMARK, true, target);
 
-    /**
-     * Unlike `@Injectable()`, in Nest,
-     * the presence of the `@Controller()` decorator is essential
-     * for defining request-handling controller classes.
-     */
-    Controller(prefixOrOptions)(target as new (...args: any[]) => any);
+    const decorator = typeof prefixOrOptions === 'string'
+      ? Controller(prefixOrOptions)
+      : Controller(prefixOrOptions as ControllerOptions);
+
+    decorator(target as new (...args: any[]) => any);
   };
-}
+};
