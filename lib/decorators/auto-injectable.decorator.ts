24d53b7f2d3071fbb272b6e5a12c3f77aebe72e5
Nhahan <kisy324@naver.com>
2023-08-12 00:59:33 +0900

    fix: `AutoInjectable` has `Reflect.defineMetadata(INJECTABLE_WATERMARK, true, target);`

--- lib/decorators/auto-injectable.decorator.ts
+++ lib/decorators/auto-injectable.decorator.ts
@@ -1,8 +1,10 @@
 import { AUTO_INJECTABLE_WATERMARK } from '../interfaces';
 import 'reflect-metadata';
+import { INJECTABLE_WATERMARK } from '@nestjs/common/constants';
 
 export function AutoInjectable(): ClassDecorator {
   return (target: object) => {
+    Reflect.defineMetadata(INJECTABLE_WATERMARK, true, target);
     Reflect.defineMetadata(AUTO_INJECTABLE_WATERMARK, true, target);
   };
 }
