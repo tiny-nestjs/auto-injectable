3a3e2e6b94cd5a624a2a8f6fb88533ae046612ed
player31-kks <keumks789@naver.com>
2023-08-13 21:44:55 +0900

    fix: Module Configuration Method

--- lib/auto.module.ts
+++ lib/auto.module.ts
@@ -1,10 +1,22 @@
 import { DynamicModule, Module } from '@nestjs/common';
 import { Importer } from './core';
+import { ImporterSync } from './core/importSync';
 
 @Module({})
 export class AutoModule {
   static async forRootAsync(patterns: string[]): Promise<DynamicModule> {
     const classes = await Importer.load(patterns);
+
+    return {
+      module: AutoModule,
+      controllers: [...classes.controllers],
+      providers: [...classes.providers],
+      exports: [...classes.providers],
+    };
+  }
+
+  static forRoot(patterns: string[]): DynamicModule {
+    const classes = ImporterSync.load(patterns);
 
     return {
       module: AutoModule,
