13e36d8628a86a409c18eb640b71eddcd3ca8f96
Nhahan <kisy324@naver.com>
2023-08-15 22:40:59 +0900

    fix: `@AutoAlias` proper parameter

--- lib/decorators/auto-alias.decorator.ts
+++ lib/decorators/auto-alias.decorator.ts
@@ -1,8 +1,7 @@
-import { InjectableOptions } from '@nestjs/common/decorators/core/injectable.decorator';
 import { AUTO_ALIAS_WATERMARK } from '../interfaces';
 
-export function AutoAlias(options?: InjectableOptions) {
+export function AutoAlias(alias?: string) {
   return (target: object) => {
-    Reflect.defineMetadata(AUTO_ALIAS_WATERMARK, true, target);
+    Reflect.defineMetadata(AUTO_ALIAS_WATERMARK, alias, target);
   };
 }
