6ebf16b508555f7655d9540efa9e3b8a20e0796b
Nhahan <kisy324@naver.com>
2023-08-18 01:14:48 +0900

    add: define reflection watermark on `ComponentScan`

--- lib/decorators/component-scan.decorator.ts
+++ lib/decorators/component-scan.decorator.ts
@@ -1,9 +1,12 @@
 import { AutoModule } from '../auto.module';
 import { Module } from '@nestjs/common';
-import * as path from 'path';
+import { COMPONENT_SCAN_WATERMARK } from '../interfaces';
+import { resolve } from 'path';
 
 export function ComponentScan(pathParameter?: string | string[]): ClassDecorator {
   return function (target: any) {
+    Reflect.defineMetadata(COMPONENT_SCAN_WATERMARK, true, target);
+
     const originalMetadata = {
       imports: Reflect.getMetadata(MODULE_OPTIONS.IMPORTS, target) || [],
       controllers: Reflect.getMetadata(MODULE_OPTIONS.CONTROLLERS, target) || [],
@@ -27,20 +30,19 @@ function getForRootPath(pathParameter?: string | string[]): string[] {
   return Array.isArray(pathParameter) ? pathParameter.map(toForRootPath) : [pathParameter].map(toForRootPath);
 }
 
-function toForRootPath(path: string) {
-  return `${require.main?.path || process.cwd()}/**/${path}/**/*.js`;
-}
-
 function getFolderPath(): string {
   const stackTrace = new Error().stack;
   const filePathRegex = /at __decorate * \((.*\.js)/;
   const matches = stackTrace?.match(filePathRegex);
   if (matches && matches.length > 1) {
-    const filePath = matches[1];
-    const matchedPath = path.join(path.resolve(filePath), '..');
+    const matchedPath = resolve(matches[1], '..');
     return `${matchedPath}/**/*.js`;
   }
   throw new Error('Could not extract file path from stack trace');
+}
+
+function toForRootPath(path: string) {
+  return `${require.main?.path || process.cwd()}/**/${path}/**/*.js`;
 }
 
 const MODULE_OPTIONS = {
